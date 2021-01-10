95406993c60d611feb62fd4bc02844581b846881
brendan <2bndy5@gmail.com>
2021-01-09 20:46:31 -0800

    fix doxygen syntax; rename job in workflow

--- CirquePinnacle.h
+++ CirquePinnacle.h
@@ -248,18 +248,16 @@ struct AbsoluteReport
     uint8_t buttons;
     /**
      * @brief This will always be in range 0 <= `x` <= 2047
-     * @rst
-     * .. tip:: The datasheet recommends this value should be
-     *     clamped to range 128 <= `x` <= 1920 for reliability.
-     * @endrst
+     *
+     * The datasheet recommends this value should be
+     * clamped to range 128 <= `x` <= 1920 for reliability.
      */
     uint16_t x;
     /**
      * @brief This will always be in range 0 <= `y` <= 1535
-     * @rst
-     * .. tip:: The datasheet recommends this value should be
-     *     clamped to range 64 <= `y` <= 1472 for reliability.
-     * @endrst
+     *
+     * The datasheet recommends this value should be
+     * clamped to range 64 <= `y` <= 1472 for reliability.
      */
     uint16_t y;
     /**
