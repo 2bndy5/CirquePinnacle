29c40d041815b22a296cfd069325964674d45ed0
Brendan <2bndy5@gmail.com>
2023-04-15 14:13:17 -0700

    review default pins used in Pico SDK examples

--- examples/pico_sdk/defaultPins.h
+++ examples/pico_sdk/defaultPins.h
@@ -7,7 +7,6 @@
 #endif
 
 #if defined(ADAFRUIT_QTPY_RP2040)
-    // for this board, you can still use the Stemma QT connector as a separate I2C bus (`i2c1`)
     #define SS_PIN PICO_DEFAULT_UART_RX_PIN // the pin labeled RX (GPIO5)
     #ifndef DR_PIN // if not using PINNACLE_SW_DR
         #define DR_PIN PICO_DEFAULT_UART_TX_PIN // the pin labeled TX (GPIO20)
@@ -16,13 +15,13 @@
 #elif defined(PIMORONI_TINY2040)
     #define SS_PIN PICO_DEFAULT_SPI_CSN_PIN // pin 5
     #ifndef DR_PIN // if not using PINNACLE_SW_DR
-        #define DR_PIN PICO_DEFAULT_I2C_SDA_PIN // pin 2
+        #define DR_PIN 1 // pin 1
     #endif
 
 #elif defined(SPARFUN_THINGPLUS)
-    #define SS_PIN 16 // the pin labeled 16
+    #define SS_PIN 21 // the pin labeled 21
     #ifndef DR_PIN // if not using PINNACLE_SW_DR
-        #define DR_PIN 6 // the pin labeled SDA
+        #define DR_PIN 22 // the pin labeled 22
     #endif
 
 #elif defined(ADAFRUIT_ITSYBITSY_RP2040)
