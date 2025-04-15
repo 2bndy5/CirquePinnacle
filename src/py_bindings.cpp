0715548ae66b33ca189a08f465e195ead494e260
Brendan <2bndy5@gmail.com>
2025-04-14 19:08:54 -0700

    update copyright year

--- src/py_bindings.cpp
+++ src/py_bindings.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Brendan Doherty (2bndy5)
+ * Copyright (c) 2025 Brendan Doherty (2bndy5)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
